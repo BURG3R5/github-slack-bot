a4d358a36133d062f76692cd2b86469522794664
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2021-12-31 12:38:33 +0530

    docs: Add docstrings to `slack.py`.

--- models/slack.py
+++ models/slack.py
@@ -1,10 +1,25 @@
+"""
+Collection of models related to the Slack portion of the bot.
+"""
 from models.github import EventType
 
 
 class Channel:
+    """
+    Model for a Slack channel with event subscriptions.
+
+    :param name: The channel name, including the "#".
+    :param events: `set` of events the channel has subscribed to.
+    """
+
     def __init__(self, name: str, events: set[EventType]):
         self.name = name
         self.events = events
 
     def is_subscribed_to(self, event: EventType) -> bool:
+        """
+        Wrapper for `__contains__` to make the use and result more evident.
+        :param event: EventType to be checked.
+        :return: Whether the channel is subscribed to the passed event or not.
+        """
         return event in self.events
