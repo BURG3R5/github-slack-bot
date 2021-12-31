9b9cdfd5268a3e307386bd5b40042d344f1a50fb
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2021-12-31 12:23:34 +0530

    docs: Add docstrings to `link.py`.

--- models/link.py
+++ models/link.py
@@ -1,10 +1,26 @@
-from typing import Optional
+"""
+Contains the `Link` model.
+
+This was separated from "slack.py" To prevent circular-import error.
+"""
 
 
 class Link:
-    def __init__(self, url: Optional[str] = None, text: Optional[str] = None):
+    """
+    Holds a text string and a URL.
+    Has an overridden __str__ method to make posting links on Slack easier.
+
+    :param url: URL that the link should lead to.
+    :param text: Text that should be displayed instead of the link.
+    """
+
+    def __init__(self, url: str = '', text: str = ''):
         self.url = url
         self.text = text
 
     def __str__(self) -> str:
+        """
+        Overridden object method for pretty-printing links.
+        :return: String formatted like a proper Slack link.
+        """
         return f"<{self.url}|{self.text}>"
