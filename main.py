bc3542088dffedb21be60cb1a5b1dcb344ce127e
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-02-19 20:19:17 +0530

    docs: Add docstrings.

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+"""
+Execution entrypoint for the github-slack-bot project.
+
+Sets up a `bottle` server with three endpoints: "/", "/github/events" and "/slack/commands".
+
+"/" is used for testing and status checks.
+
+"/github/events" is provided to GitHub Webhooks to POST event info at.
+Triggers `manage_github_events` which uses `GitHubPayloadParser.parse` and `SlackBot.inform`.
+
+"/slack/commands" is provided to Slack to POST slash command info at.
+Triggers `manage_slack_commands` which uses `SlackBot.run`.
+"""
+from typing import Any
+
 from bottle import post, run, request, get
 
 from github_parsers import GitHubPayloadParser
@@ -6,20 +21,36 @@ from slack_bot import SlackBot
 
 
 @get("/")
-def test_get():
+def test_get() -> str:
+    """
+    First test endpoint.
+    :return: Plaintext confirming server status.
+    """
     return "This server is running!"
 
 
 @post("/")
-def test_post():
+def test_post() -> str:
+    """
+    Second test endpoint.
+    :return: Status confirmation plaintext containing name supplied in request body.
+    """
+    try:
+        name: str = request.json["name"]
+    except KeyError:
+        name: str = "unknown"
     return (
         f"This server is working, and to prove it to you, "
-        f"I'll guess your name!\nYour name is... {request.json['name']}!"
+        f"I'll guess your name!\nYour name is... {name}!"
     )
 
 
 @post("/github/events")
-def manage_github_events():
+def manage_github_events() -> None:
+    """
+    Uses `GitHubPayloadParser` to parse and cast the payload into a `GitHubEvent`.
+    Then uses an instance of `SlackBot` to send appropriate messages to appropriate channels.
+    """
     event: GitHubEvent | None = GitHubPayloadParser.parse(
         event_type=request.headers["X-GitHub-Event"],
         raw_json=request.json,
@@ -30,9 +61,16 @@ def manage_github_events():
 
 @post("/slack/commands")
 def manage_slack_commands() -> dict | None:
-    response: dict | None = bot.run(raw_json=request.forms)
+    """
+    Uses a `SlackBot` instance to run the slash command triggered by the user.
+    Optionally returns a Slack message dict as a reply.
+    :return: Appropriate response for sent slash command in the form of a dict.
+    """
+    # Unlike GitHub webhooks, Slack does not send the data in `requests.json`.
+    # Instead, the data is passed in `request.forms`.
+    response: dict[str, Any] | None = bot.run(raw_json=request.forms)
     return response
 
 
 bot: SlackBot = SlackBot()
-run(host="", port=5556, debug=True, reloader=True)
+run(host="", port=5556, debug=True)
--- models/link.py
+++ models/link.py
@@ -1,7 +1,26 @@
+"""
+Contains the `Link` model.
+
+This was separated from "slack.py" To prevent circular-import error.
+"""
+
+
 class Link:
+    """
+    Holds a text string and a URL.
+    Has an overridden `__str__` method to make posting links on Slack easier.
+
+    :param url: URL that the link should lead to.
+    :param text: Text that should be displayed instead of the link.
+    """
+
     def __init__(self, url: str | None = None, text: str | None = None):
         self.url = url
         self.text = text
 
     def __str__(self) -> str:
+        """
+        Overridden object method for pretty-printing links.
+        :return: String formatted like a proper Slack link.
+        """
         return f"<{self.url}|{self.text}>"
