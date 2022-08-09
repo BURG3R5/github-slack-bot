3bdb0682ba7770e6d5f1670bad6d281f318a3122
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-08-09 23:05:53 +0530

    fix(test): Fix serializer to properly parse iterators.

--- tests/utils/serializers.py
+++ tests/utils/serializers.py
@@ -6,5 +6,8 @@ from bot.models.github.event import GitHubEvent
 def github_event_serializer(github_event: GitHubEvent) -> dict[str, Any]:
     serialized = {}
     for var, value in vars(github_event).items():
-        serialized[var] = str(value)
+        if isinstance(value, (list, tuple, set)):
+            serialized[var] = [str(v) for v in value]
+        else:
+            serialized[var] = str(value)
     return serialized
