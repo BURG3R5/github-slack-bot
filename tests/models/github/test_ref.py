9054d430f39fe2fbeb275ecea000aec74a3381e4
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2022-11-26 05:42:26 +0530

    feat(models): Add unittest for ref model.

--- tests/models/github/test_ref.py
+++ tests/models/github/test_ref.py
@@ -6,5 +6,5 @@ from bot.models.github.ref import Ref
 class RefTest(unittest.TestCase):
 
     def test_should_create_Ref(self):
-        ref = Ref(name="hey this is unittest ref", ref_type="branch")
-        self.assertEqual(str(ref), "hey this is unittest ref")
+        ref = Ref(name="hey, this is unittest ref", ref_type="branch")
+        self.assertEqual(str(ref), "hey, this is unittest ref")
